0190b951d260cf9062279a9c225a8a73051e5e59
Emily Jiang <brilliantemilyjiang@gmail.com>
2020-11-18 18:26:28 -0500

    Update Prime.cs

--- PrimeChecker/Prime.cs
+++ PrimeChecker/Prime.cs
@@ -8,8 +8,8 @@ namespace PrimeChecker
     {
         public static bool Check(int num)
         {
-            //if (num <= 1)
-            //    return false;
+            if (num <= 1)
+                return false;
             for (int i = 2; i < num; i++)
             {
                 if (num % i == 0)
